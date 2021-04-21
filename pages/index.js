96401a3b0d84d699d964a271c394ed8ee51bf807
leticijak <mnogomijeheavy@gmail.com>
2021-04-21 13:17:30 +0200

    Layout, Header, Footer

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
 import Layout from '../components/Layout'
 
 export default function HomePage() {
   return (
     <Layout>
-      <h1> HOME </h1>
+      <h1>HOME</h1>
+      <h3>Hello My name is dr.GreenThomb</h3>
     </Layout>
   )
 }
