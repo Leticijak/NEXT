84a1aa680a31ed8f5d169cc695718836d529cbf8
ben <mnogomijeheavy@gmail.com>
2021-04-21 11:49:21 +0200

    Pages and routing

---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+  return (
+    <div>
+      <h1>Ovo je moja objava</h1>
+    </div>
+  )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+export default function EventsPage() {
+  return (
+    <div>
+      <h1>My Events</h1>
+      <h1></h1>
+      <h2></h2>
+    </div>
+  )
+}
